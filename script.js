f20bb0b824e135d00b950811e6080e58b8fee3eb
ABWEBIT <108275652+ABWEBIT@users.noreply.github.com>
2022-07-01 10:52:55 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let textAreaAutoHeight = () => {
+  var textArea = document.getElementsByClassName('textAreaAutoHeight');
+  for(let i = 0; i < textArea.length; i++){ 
+    let element,tStyle,tHeight,tPadTop,tPadBot,tBorTop,tBorBot,tLineHeight;
+
+    element = textArea[i];
+    tStyle = window.getComputedStyle(element);
+    tHeight = parseFloat(tStyle.height);
+    tPadTop = parseFloat(tStyle.paddingTop);
+    tPadBot = parseFloat(tStyle.paddingBottom);
+    tBorTop = parseFloat(tStyle.borderTop);
+    tBorBot = parseFloat(tStyle.borderBottom);
+    tLineHeight = tHeight - tPadTop - tPadBot - tBorTop - tBorBot;
+
+    element.addEventListener('input', function(){
+      this.rows = 1;
+      this.rows = Math.floor((this.scrollHeight - tPadTop - tPadBot) / tLineHeight);
+    });
+  };
+};
+textAreaAutoHeight();
