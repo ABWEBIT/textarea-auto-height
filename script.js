952931f77788e4fd5b701f196d0af5f550f792bd
ABWEBIT <108275652+ABWEBIT@users.noreply.github.com>
2022-07-01 12:58:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 let textAreaAutoHeight = () => {
   var textArea = document.getElementsByClassName('textAreaAutoHeight');
   for(let i = 0; i < textArea.length; i++){ 
-    let element,tStyle,tHeight,tPadTop,tPadBot,tBorTop,tBorBot,tLineHeight;
+    let tStyle,tHeight,tPadding,tBorder,tRowHeight;
 
-    element = textArea[i];
-    tStyle = window.getComputedStyle(element);
+    tStyle = window.getComputedStyle(textArea[i]);
     tHeight = parseFloat(tStyle.height);
-    tPadTop = parseFloat(tStyle.paddingTop);
-    tPadBot = parseFloat(tStyle.paddingBottom);
-    tBorTop = parseFloat(tStyle.borderTop);
-    tBorBot = parseFloat(tStyle.borderBottom);
-    tLineHeight = tHeight - tPadTop - tPadBot - tBorTop - tBorBot;
+    tPadding = parseFloat(tStyle.paddingTop) + parseFloat(tStyle.paddingBottom);
+    tBorder = parseFloat(tStyle.borderTop) + parseFloat(tStyle.borderBottom);
+    tRowHeight = tHeight - tPadding - tBorder;
 
-    element.addEventListener('input', function(){
+    textArea[i].addEventListener('input', function(){
       this.rows = 1;
-      this.rows = Math.floor(this.scrollHeight / tLineHeight);
+      this.rows = Math.floor(this.scrollHeight / tRowHeight);
     });
   };
 };
