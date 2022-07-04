bdb3e2ee1a654e9decf9f91cc9d0d08c5bd9b08a
ABWEBIT <108275652+ABWEBIT@users.noreply.github.com>
2022-07-04 20:13:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 'use strict';
 let textAreaAutoHeight = () => {
   let textArea = document.getElementsByClassName('textAreaAutoHeight');
-  for(let i = 0; i < textArea.length; i++){ 
+  for(let el of textArea){ 
     let tStyle,tHeight,tPadding,tBorder,tRowHeight;
 
-    tStyle = window.getComputedStyle(textArea[i]);
+    tStyle = window.getComputedStyle(el);
     tHeight = parseFloat(tStyle.height);
     tPadding = parseFloat(tStyle.paddingTop) + parseFloat(tStyle.paddingBottom);
     tBorder = parseFloat(tStyle.borderTop) + parseFloat(tStyle.borderBottom);
     tRowHeight = tHeight - tPadding - tBorder;
 
-    textArea[i].addEventListener('input', function(){
+    el.addEventListener('input', function(){
       this.rows = 1;
       this.rows = Math.round((this.scrollHeight - tPadding) / tRowHeight);
     });
