f495efce4227bee96ca8d7d842276abf113ffc3e
ABWEBIT <108275652+ABWEBIT@users.noreply.github.com>
2024-12-16 17:16:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-'use strict';
-class Textarea{
-  constructor(elementId,rowsMax){
-    this.element = document.getElementById(elementId);
-    this.element.value = '';
-    this.rowsMax = rowsMax;
-    this.rowsCount = 0;
 
-  };
-  textareaAutoHeight(){
-    this.element.rows = 1;
-    this.rowsCount = Math.round((this.element.scrollHeight - this.padding) / this.rowHeight);
-
-  };
-}
-new Textarea('chat',4);
-// textarea id, maximum rows >= 0 (0 for infinity)
